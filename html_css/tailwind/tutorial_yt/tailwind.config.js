fc2a654403b422fa1ddb9166e5811fb403de2290
hiramiya <hiramiya523@gmail.com>
2022-09-21 22:18:26 +0900

    tailwind background image

--- html_css/tailwind/tutorial_yt/tailwind.config.js
+++ html_css/tailwind/tutorial_yt/tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    // 利用したいimageを以下のように追記
+    backgroundImage: {
+      "tutorial-bg" : "url('../src/img/main-bg.jpg')",
+    },
   },
   plugins: [],
 }
---
+++ sainoto/css/tailwind.md
@@ -0,0 +1,15 @@
+cssフレームワーク
+
+
+# 環境構築
+- npm init -y
+- npm install -D tailwindcss
+  - npm i -D tailwindcss
+- npx tailwindcss init
+
+- npm run build 
+  定義したbuildコマンドをrunする
+
+
+# メリット
+クラス名を考える時間がなくなる
